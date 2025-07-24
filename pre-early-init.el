8e9ed0f216c0d6ef2120be41976c11dc13b0add4
Dean Sellis <dean.sellis@gmail.com>
2025-07-24 22:38:46 +0100

    Added additional config file and updated readme

---
+++ pre-early-init.el
@@ -0,0 +1,16 @@
+(setq minimal-emacs-user-directory "~/.config/minimal-emacs.d/")
+
+
+(when (featurep 'native-compile)
+  ;; Ensure Emacs consistently uses the specified ELN cache directory for native
+  ;; compilation, preventing it from creating or loading .eln files in the
+  ;; default system or user-standard paths. This guarantees that both loading
+  ;; and writing of native-compiled files happen exclusively in the user-defined
+  ;; cache directory, avoiding permission issues and path inconsistencies.
+  (let ((eln-cache-dir (convert-standard-filename
+                        (expand-file-name "eln-cache" user-emacs-directory))))
+    (when (boundp 'native-comp-eln-load-path)
+      (setcar native-comp-eln-load-path eln-cache-dir))
+    (setq native-compile-target-directory eln-cache-dir)
+    (when (fboundp 'startup-redirect-eln-cache)
+      (startup-redirect-eln-cache eln-cache-dir))))
